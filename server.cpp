f76fab8300923b09791de52fe6b00775e9a34a3f
Brown Teague <dbt0013@auburn.edu>
2022-12-01 21:07:02 -0600

    nak bzero

--- server.cpp
+++ server.cpp
@@ -278,6 +278,7 @@ int main(int argc, char *argv[]) {
 
                 else if (msg == "NAK") {
                     lastAck = seqNum;
+                    bzero(&receiveBuffer, BUFFSIZE);
                     break;
                 }
 
